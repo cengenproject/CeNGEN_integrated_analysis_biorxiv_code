0b4209667d2751804efddf74b789fd43533f5e13
Alec Barrett <49766873+alecbarrett@users.noreply.github.com>
2022-01-13 13:35:56 -0800

    Update Length_normalized_integration.r
    Updated to subset replicates, only keeping those with more than one replicate in both the single cell and bulk datasets

--- Integration/Length_normalized_integration.r
+++ Integration/Length_normalized_integration.r
@@ -11,13 +11,9 @@ remotes::install_github("AlexWeinreb/wbData")
 library(stringr)
 library(wbData)
 library(edgeR)
+library(pbapply)
 
 ## function for integration
-
-### inputs:
-# bulk genes x samples matrix. Colnames should be: cell_type#sampleID, where # is in place of some string to split on. default currently is lower case r
-# single cell pseudobulk genes x samples matrix Colnames should be: cell_type#sampleID, where # is in place of some string to split on. default currently is two underscores
-# pseudocount, no default given, I use 0.1 to reduce distortion of non-zero counts
 
 integrate_geometricMean_biorep <- function(bulk_replicates, single_cell_replicates, pseudocount, bulk_sep = 'r', single_cell_sep = '__'){
 
@@ -32,8 +28,8 @@ integrate_geometricMean_biorep <- function(bulk_replicates, single_cell_replicat
   index_list_sc <- numeric()
   index_list_bulk <- numeric()
   for(g in unique(bulk_cell_types)){
-    rep_totals <- c(sum(bulk_cell_types==g), sum(cells_bio_rep==g))### count how many replicates are in each cell type for each data source
-    rep_min <- min(rep_totals) ## select the smaller number, this will be the number of samples in the final integrated matrixes
+    rep_totals <- c(sum(bulk_cell_types==g), sum(cells_bio_rep==g))
+    rep_min <- min(rep_totals)
     #print(g)
     #print(sum(bulk_cell_types==g))
     #print(rep_min)
@@ -49,7 +45,7 @@ integrate_geometricMean_biorep <- function(bulk_replicates, single_cell_replicat
   #print('set indexes')
   bulk_replicates_match <- bulk_replicates[,index_list_bulk]
 
-  single_cell_replicates_match <- single_cell_replicates[,index_list_sc] ### normalize the Single Cell data to match the bulk data
+  single_cell_replicates_match <- single_cell_replicates[,index_list_sc]
   single_cell_replicates_match <- sweep(single_cell_replicates_match, 2, colSums(single_cell_replicates_match, na.rm = T), '/') * colSums(bulk_replicates_match, na.rm = T)
 
   #bulk_replicates_match <- bulk_replicates_match[,order(colnames(bulk_replicates_match))]
@@ -59,27 +55,28 @@ integrate_geometricMean_biorep <- function(bulk_replicates, single_cell_replicat
 
   sample_level_integration <- exp( ( log(bulk_replicates_match + pseudocount) +
                                        log(single_cell_replicates_match + pseudocount) ) /2 ) - pseudocount
-  
-  sample_level_integration <- sample_level_integration[order(rownames(sample_level_integration)),
-                                                       order(colnames(sample_level_integration))]
 
-  return(sample_level_integration)
-  }
+  return(sample_level_integration)}
 
 
-## load single cell aggregates, precomputed. Each is the sum of counts for cells in a cluster-replicate (ex: all AFD cells that came from replicate cho.1_2)
-## limited to cluster-replicates with >= 10 cells
+## load single cell aggregates
 
-CeNGEN_aggr_bio_reps <- read.table('~/Bioinformatics/single_cell_data/CeNGEN_aggr_bio_reps_101821.tsv')
-
-print('loaded single cell reps')
-
-
+CeNGEN_aggr_bio_reps <- read.table('CeNGEN_aggr_bio_reps_101821.tsv')
 
 CeNGEN_aggr_bio_reps <- CeNGEN_aggr_bio_reps[,order(colnames(CeNGEN_aggr_bio_reps))]
 
 
-### expand to VD and DD, with the caveat that we then can't do differential expression comparing VD and DD using this dataset
+print('loaded single cell reps')
+
+## load bulk data
+
+bulk_data <- read.table('bsn9_CeNGEN_bulk_data.tsv', header = T)
+
+
+
+
+## fix the single cell data to match bulk_data labels
+
 cell_labels <- str_split_fixed(colnames(CeNGEN_aggr_bio_reps), '__',2)[,1]
 CeNGEN_aggr_bio_reps_DD.temp <- CeNGEN_aggr_bio_reps[,cell_labels == 'VD_DD']
 CeNGEN_aggr_bio_reps_VD.temp <- CeNGEN_aggr_bio_reps[,cell_labels == 'VD_DD']
@@ -92,17 +89,12 @@ CeNGEN_aggr_bio_reps <- CeNGEN_aggr_bio_reps.test[,order(colnames(CeNGEN_aggr_bi
 cell_labels <- str_split_fixed(colnames(CeNGEN_aggr_bio_reps), '__',2)[,1]
 
 
+
+## get bulk data labels
 bulk_cell_types <- str_split_fixed(colnames(bulk_data), 'r', 2)[,1]
 
 
-### get the bulk metadata, to normalize genes by length prior to integration, if desired.
-### not recommended if performing differential expression! This is primarily for increased accuracy in calling expression.
-
-bulk_meta <- read.table('~/Bioinformatics/bsn5/bsn5_bulk_counts_metadata.tsv')
-bulk_data_pk <- (bulk_data/bulk_meta[rownames(bulk_data), 'Length']) * 1000
-
-
-## which cell types have > 1 replicate? Performed for both bulk and pseudobulk replicates
+## which cell types have > 1 replicate?
 
 samples <- colnames(bulk_data)
 samples_cell_types <- do.call(rbind, strsplit(colnames(bulk_data), 'r', 2))[,1]
@@ -111,54 +103,46 @@ cell_types_wReps <- cell_types[base::table(samples_cell_types) > 1]
 samples_wReps <- samples[samples_cell_types %in% cell_types_wReps]
 samples_wReps_cell_types <- do.call(rbind, strsplit(samples_wReps, 'r', 2))[,1]
 
-samples_SC <- colnames(CeNGEN_aggr_bio_reps)
-samples_cell_types_SC <- do.call(rbind, strsplit(colnames(CeNGEN_aggr_bio_reps), '__', 2))[,1]
-cell_types_SC <- unique(samples_cell_types_SC)
-cell_types_wReps_SC <- cell_types_SC[base::table(samples_cell_types_SC) > 1]
-samples_wReps_SC <- samples_SC[samples_cell_types_SC %in% cell_types_wReps_SC]
-samples_wReps_cell_types_SC <- do.call(rbind, strsplit(samples_wReps_SC, '__', 2))[,1]
+
+sc_samples <- colnames(CeNGEN_aggr_bio_reps)
+sc_samples_cell_types <- do.call(rbind, strsplit(colnames(CeNGEN_aggr_bio_reps), '__', 2))[,1]
+sc_cell_types <- unique(sc_samples_cell_types)
+sc_cell_types_wReps <- sc_cell_types[base::table(sc_samples_cell_types) > 1]
+sc_samples_wReps <- sc_samples[sc_samples_cell_types %in% sc_cell_types_wReps]
+sc_samples_wReps_cell_types <- do.call(rbind, strsplit(sc_samples_wReps, '__', 2))[,1]
+
+bulk_data_use <- bulk_data[,samples_wReps]
+CeNGEN_aggr_bio_reps_use <- CeNGEN_aggr_bio_reps[,sc_samples_wReps]
+
+
+## subset down to just the samples with > 1 replicate in bulk and single cell
+
+combined_cell_types <- intersect(unique(samples_wReps_cell_types), unique(sc_samples_wReps_cell_types))
+
+bulk_data_use <- bulk_data_use[, str_split_fixed(colnames(bulk_data_use), 'r', 2)[,1] %in% combined_cell_types]
+CeNGEN_aggr_bio_reps_use <- CeNGEN_aggr_bio_reps_use[, str_split_fixed(colnames(CeNGEN_aggr_bio_reps_use), '__', 2)[,1] %in% combined_cell_types]
+
+
+
+## normalize bulk data to gene length
+
+bulk_data_use <- (bulk_data_use/bulk_meta[rownames(bulk_data_use), 'Length']) * 1000
 
 
 
 ### integrate them, and if desired, save them
 
-many_integrations_pk <- pblapply(seq(101,150,1), function(v){
+many_integrations <- pblapply(seq(101,150,1), function(v){
   set.seed(v)
-  test_int <- integrate_geometricMean_biorep(bulk_data_pk[,samples_wReps], CeNGEN_aggr_bio_reps[,samples_wReps_SC],
+  test_int <- integrate_geometricMean_biorep(bulk_data_use, CeNGEN_aggr_bio_reps_use,
                           pseudocount = 0.1, bulk_sep = 'r', single_cell_sep = '__')
   return(test_int) })
 print('integrated samples')
 
-colnames(many_integrations_pk[[1]])
+many_integrations[[1]][1:10,1:10]
 
-
-
-#### TMM normalize the integrated samples
-many_GeTMMs <- pblapply(many_integrations_pk, function(integrant){
-  GeTMM <- DGEList(integrant)
-  GeTMM <- calcNormFactors(GeTMM)
-  GeTMM <- cpm(GeTMM, normalized.lib.sizes = T)
-  GeTMM[GeTMM < 0.0000000000001] <- 0
-  return(GeTMM)
-})
-many_GeTMMs[[1]][1:10,1:10]
-bulk_raw_GeTMM[1:10,1:10]
-
-
-### get the average normalized samples
-ave_integrated_GeTMM <- Reduce('+', many_GeTMMs)/length(many_GeTMMs)
-ave_integrated_GeTMM <- ave_integrated_GeTMM[order(rownames(ave_integrated_GeTMM)),
-                                                             order(colnames(ave_integrated_GeTMM))]
-                                                             
-### get the average cell_type profile                                                             
-aggr_ave_integrated_GeTMM <- ave_integrated_GeTMM
-colnames(aggr_ave_integrated_GeTMM) <-str_split_fixed(colnames(aggr_ave_integrated_GeTMM),"r",2)[,1]
-aggr_ave_integrated_GeTMM <- data.frame(vapply(unique(colnames(aggr_ave_integrated_GeTMM)), function(x) 
-  rowMeans(aggr_ave_integrated_GeTMM[,colnames(aggr_ave_integrated_GeTMM)== x,drop=FALSE], na.rm=TRUE),
-  numeric(nrow(aggr_ave_integrated_GeTMM)) ))
-
-
-### save the integrated matrixes if desired. Here written as separate csvs. Easy to change if you want to save as 1 RDS file.
-for(g in seq(1,50,1)){
+for(g in seq(1,3,1)){
   write.csv(many_integrations1[[g]], paste0('integrated_matrix_', g, '.csv'))
 }
+
+
