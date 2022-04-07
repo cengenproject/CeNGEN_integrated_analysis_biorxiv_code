2734ce22729223a0c300a714a4363678749227a9
Alec Barrett <49766873+alecbarrett@users.noreply.github.com>
2022-04-07 17:23:54 -0400

    Update and rename TPR_FPR_FDR_calculation.R to TPR_FPR_FDR_calculation (Figure 2B_C_D).R

--- Analysis/TPR_FPR_FDR_calculation (Figure 2B_C_D).R
+++ Analysis/TPR_FPR_FDR_calculation (Figure 2B_C_D).R
@@ -59,14 +59,14 @@ CeNGEN_TPM <- read.table('CeNGEN_TPM_080421.tsv')
 CeNGEN_TPM <- CeNGEN_TPM[order(rownames(CeNGEN_TPM)), order(colnames(CeNGEN_TPM))]
 
 
-
-bulk_data <- read.table('bsn9_bulk_counts_113021.tsv', sep = '\t')
+# ~~ Bulk data load ----
+bulk_data <- read.table('Barrett_et_al_2022_CeNGEN_bulk_RNAseq_data.tsv', sep = '\t')
 
 ## if present, remove outliers and cell types with only 1 replicate
-bulk_load$RICr133 <- NULL
-bulk_load$PVMr122 <- NULL
-bulk_load$ADFr99 <- NULL
-bulk_load$M4r117 <- NULL
+bulk_data$RICr133 <- NULL
+bulk_data$PVMr122 <- NULL
+bulk_data$ADFr99 <- NULL
+bulk_data$M4r117 <- NULL
 
 
 
@@ -82,7 +82,7 @@ bulk_data <- bulk_data[genes_keep,]
 
 
 ## load gene metadata table
-bulk_meta <- read.table('bsn9_CeNGEN_bulk_geneLevel_metadata.tsv', sep = '\t')
+bulk_meta <- read.table('Barrett_et_al_2022_CeNGEN_bulk_RNAseq_geneLevel_metadata.tsv')
 
 #### normalize to gene length
 bulk_data_pk <- (bulk_data/bulk_meta[rownames(bulk_data),'Length'])*1000
@@ -99,22 +99,8 @@ bulk_raw_GeTMM <- cpm(bulk_raw_GeTMM, normalized.lib.sizes = T)
 
 
 
-
-
-### only consider cell types with samples in the integrated dataset
-bulk_samples <- colnames(average_integration_pk)
-bulk_sample_cell_types <- str_split_fixed(colnames(average_integration_pk), 'r', 2)[,1]
-cell_types <- unique(bulk_sample_cell_types)
-cell_types_wReps <- cell_types[base::table(bulk_sample_cell_types) > 1]
-samples_wReps <- bulk_samples[bulk_sample_cell_types %in% cell_types_wReps]
-samples_wReps_cell_types <- str_split_fixed(samples_wReps, 'r', 2)[,1]
-
-
-
-# Make conformable gene list & neuron list
+# Make conformable gene list
 common.genes <- intersect(rownames(average_integration_GeTMM), rownames(CeNGEN_TPM))
-
-bulk_neurs <- base::intersect(colnames(all_genes_bulk), cell_types_wReps)
 
 ###~~  average within cell types ----
 
@@ -130,9 +116,6 @@ colnames(aggr_ave_integrant_Getmm) <-str_split_fixed(colnames(aggr_ave_integrant
 aggr_ave_integrant_Getmm <- data.frame(vapply(unique(colnames(aggr_ave_integrant_Getmm)), function(x) 
   rowMeans(aggr_ave_integrant_Getmm[,colnames(aggr_ave_integrant_Getmm)== x,drop=FALSE], na.rm=TRUE),
   numeric(nrow(aggr_ave_integrant_Getmm)) ))
-
-
-
 
 
 
@@ -216,17 +199,11 @@ auc(1-diags_aggr_raw_GeTMM_plot$FDR, diags_aggr_raw_GeTMM_plot$TPR)
 
 ### load non-neuronal ground truth genes
 
-likely_non_neuronal <- read.table('simpleMine_non_neuronal_genes_101521.tsv', sep = '\t',header = T)
-likely_non_neuronal <- likely_non_neuronal[likely_non_neuronal$Putative.match.=='Yes',]
+nonNeuronal_GT <- read.csv('bulk_nonNeuronal_ground_truth_040122.csv', row.names = 1)
 
-likely_non_neuronal_gt <- data.frame(row.names = likely_non_neuronal$WormBase.Gene.ID, 
-                                     matrix(0, ncol=ncol(all_genes_bulk), nrow=nrow(likely_non_neuronal)))
-colnames(likely_non_neuronal_gt) <- colnames(all_genes_bulk)
+nn_genes <- intersect(rownames(nonNeuronal_GT), common.genes)
 
-nn_genes <- intersect(rownames(likely_non_neuronal_gt), common.genes)
-nn_genes <- intersect(nn_genes, rownames(prop_by_type_adjusted))
-
-likely_non_neuronal_gt_cut <- likely_non_neuronal_gt[nn_genes, neurons]
+nonNeuronal_GT <- nonNeuronal_GT[nn_genes, neurons]
 
 
 
